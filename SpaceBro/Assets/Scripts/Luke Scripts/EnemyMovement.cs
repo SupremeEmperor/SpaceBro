53317eb3e1eae05717c8f066bd789690ef5f988d
Luke M <lukeam@uci.edu>
2019-04-23 15:10:25 -0700

    hha

--- SpaceBro/Assets/Scripts/Luke Scripts/EnemyMovement.cs
+++ SpaceBro/Assets/Scripts/Luke Scripts/EnemyMovement.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    public GameObject pin;
+    public float speed;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        rotate();
+        move();
         
+    }
+
+    void rotate()
+    {
+        Vector3 diff = (pin.transform.position - transform.position).normalized;
+        transform.rotation = Quaternion.Euler(0f,0f,Mathf.Atan2(diff.y,diff.x) * Mathf.Rad2Deg);
+        
+        /*
+         * Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+         diff.Normalize();
+ 
+         float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+         transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+        */
+    }
+
+    private void move()
+    {
+        rb.velocity = new Vector2(0, 1 * speed);
     }
 }
