a67145fe0bd3e345f947c31814bee674980940b5
Luke M <lukeam@uci.edu>
2019-04-23 16:10:57 -0700

    EnemyMovement

--- SpaceBro/Assets/Scripts/Luke Scripts/EnemyMovement.cs
+++ SpaceBro/Assets/Scripts/Luke Scripts/EnemyMovement.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public GameObject pin;
+    public GameObject[] pin;
     public float speed;
+    public float turnRadius;
     private Rigidbody2D rb;
+    int pinNum;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); 
+        rb = GetComponent<Rigidbody2D>();
+        pinNum = 0;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        check();
         rotate();
         move();
         
@@ -24,7 +28,7 @@ public class EnemyMovement : MonoBehaviour
 
     void rotate()
     {
-        Vector3 diff = (pin.transform.position - transform.position).normalized;
+        Vector3 diff = (pin[pinNum].transform.position - transform.position).normalized;
         transform.rotation = Quaternion.Euler(0f,0f,Mathf.Atan2(diff.y,diff.x) * Mathf.Rad2Deg);
         
         /*
@@ -38,6 +42,22 @@ public class EnemyMovement : MonoBehaviour
 
     private void move()
     {
-        rb.velocity = new Vector2(0, 1 * speed);
+        rb.velocity = new Vector2(0, 0);
+        rb.AddForce(transform.right * speed);
+    }
+
+    private void check()
+    {
+        Debug.Log((pin[pinNum].transform.position - transform.position).magnitude);
+        if ((pin[pinNum].transform.position - transform.position).magnitude < turnRadius)
+        {
+            if (pinNum < (pin.Length - 1))
+            {
+                pinNum++;
+            }
+            else{
+                pin[pinNum] = GameObject.FindGameObjectWithTag("Despawner");
+            }
+        }
     }
 }
