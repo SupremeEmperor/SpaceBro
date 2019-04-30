db07117e2fe71ae0ff5c04bc6bdb4e15bd3451f9
Luke M <lukeam@uci.edu>
2019-04-30 12:06:25 -0700

    ShooterEnemy.prefab now looks at the player while moving along its path

--- SpaceBro/Assets/Scripts/Luke Scripts/ShooterRotation.cs
+++ SpaceBro/Assets/Scripts/Luke Scripts/ShooterRotation.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class ShooterRotation : MonoBehaviour
 {
+    GameObject pin;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pin = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 diff = (pin.transform.position - transform.position).normalized;
+        transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg);
     }
 }
