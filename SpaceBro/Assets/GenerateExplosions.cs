1480b7b30f65f2d0764e3a501613e145ced7cbbe
Unknown <chaseoqueso@gmail.com>
2019-04-30 16:41:02 -0700

    Explosion Generator

---
+++ SpaceBro/Assets/GenerateExplosions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateExplosions : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    public int numExplosions;
+    public float xRange;
+    public float yRange;
+    public float interval;
+
+    private float timer = 0;
+    private int explosionsLeft;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        explosionsLeft = numExplosions;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (explosionsLeft > 0)
+        {
+            if (timer > interval)
+            {
+                Instantiate(explosionPrefab, transform.position + new Vector3(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange), 0), transform.rotation);
+                new WaitForSeconds(interval);
+                --explosionsLeft;
+                timer -= interval;
+            }
+        } else {
+            Destroy(gameObject);
+        }
+    }
+}
