d7d9b2efc85b4ef9a366bdf825618750bedea116
Luke M <lukeam@uci.edu>
2019-04-30 16:35:25 -0700

    EnemyMovement updated

--- SpaceBro/Assets/Scripts/Luke Scripts/pinScript.cs
+++ SpaceBro/Assets/Scripts/Luke Scripts/pinScript.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class pinScript : MonoBehaviour
 {
     public GameObject next;
+    public float waitTime;
+    public bool vulnerable;
+    public bool shooting;
+    public float speed =100;
     // Start is called before the first frame update
     void Start()
     {
