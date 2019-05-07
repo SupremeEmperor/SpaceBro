ecd165ad05898bb0f8dc6f6d9113f1b069f2ab9d
Unknown <chaseoqueso@gmail.com>
2019-05-07 16:08:52 -0700

    Player Movement Improved and added dashing

--- SpaceBro/Assets/Scripts/Chase Scripts/Player.cs
+++ SpaceBro/Assets/Scripts/Chase Scripts/Player.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public int velInterval = 1;
+    public int maxvel = 4;
+    public int dashDelay = 30;
+    public int dashCooldown = 1;
+    public int dashSpeed = 6;
+    public int dashTime = 60;
+
     private const float deadzone = .2F;
-    private const int maxvel = 7;
     private const float forceAmount = 100f;
+    private int dashTimer = 0;
+    private int dashCooldownTimer = 0;
+    private int dashResetTimer = 0;
     private float screenWidth;
     private Rigidbody2D rb;
-    protected string state = "idle";
+
+    public string state = "idle";
 
     void Start()
     {
@@ -19,27 +29,87 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
-        if(Input.GetAxis("Horizontal") < -deadzone)
+        if(Input.GetAxisRaw("Horizontal") < -deadzone)
         {
-            state = "left";
-            if (rb.velocity.x > -maxvel)
-                rb.AddForce(new Vector2(-forceAmount, 0f));
+            if(state != "left" && state != "dashleft")
+            {
+                if(dashTimer < 0 && dashCooldownTimer == 0)
+                {
+                    dashTimer = 0;
+                    dashCooldownTimer = dashCooldown;
+                    dashResetTimer = dashTime;
+                    state = "dashleft";
+                }
+                else
+                {
+                    dashTimer = -dashDelay;
+                    state = "left";
+                }
+            }
         }
-        else if(Input.GetAxis("Horizontal") > deadzone)
+        else if(Input.GetAxisRaw("Horizontal") > deadzone)
         {
-            state = "right";
-            if (rb.velocity.x < maxvel)
-                rb.AddForce(new Vector2(forceAmount, 0f));
+            if (state != "right" && state != "dashright")
+            {
+                if (dashTimer > 0 && dashCooldownTimer == 0)
+                {
+                    dashTimer = 0;
+                    dashCooldownTimer = dashCooldown;
+                    dashResetTimer = dashTime;
+                    state = "dashright";
+                }
+                else
+                {
+                    dashTimer = dashDelay;
+                    state = "right";
+                }
+            }
         }
         else
         {
             state = "idle";
         }
-        if (state == "idle")
-            rb.drag = 20;
-        else
-            rb.drag = 0;
 
+        switch(state)
+        {
+            case "right":
+                if (rb.velocity.x < maxvel)
+                    rb.velocity += new Vector2(velInterval, 0);
+                if (rb.velocity.x > maxvel)
+                    rb.velocity -= new Vector2(velInterval, 0);
+                break;
+            case "left":
+                if (rb.velocity.x > -maxvel)
+                    rb.velocity -= new Vector2(velInterval, 0);
+                if (rb.velocity.x < -maxvel)
+                    rb.velocity += new Vector2(velInterval, 0);
+                break;
+            case "dashright":
+                rb.velocity = new Vector2(dashSpeed, 0);
+                if (dashResetTimer == 0)
+                    state = "right";
+                break;
+            case "dashleft":
+                rb.velocity = new Vector2(-dashSpeed, 0);
+                if (dashResetTimer == 0)
+                    state = "left";
+                break;
+            default:
+                if (Mathf.Abs(rb.velocity.x) != 0)
+                    rb.velocity -= new Vector2(velInterval * Mathf.Sign(rb.velocity.x), 0);
+                break;
+        }
+
+        if (dashTimer > 0)
+            --dashTimer;
+        else if (dashTimer < 0)
+            ++dashTimer;
+
+        if (dashCooldownTimer > 0)
+            --dashCooldownTimer;
+
+        if (dashResetTimer > 0)
+            --dashResetTimer;
     }
     
 }
