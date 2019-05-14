398a53e3c4810ca4ab9ff6ec81450c10694bc0f6
Luke M <lukeam@uci.edu>
2019-05-14 16:48:49 -0700

    Enemy Movement pretty much done

--- SpaceBro/Assets/Scripts/Luke Scripts/PlayerHealth.cs
+++ SpaceBro/Assets/Scripts/Luke Scripts/PlayerHealth.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public int health;
+    public int cat;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +21,14 @@ public class PlayerHealth : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Enemy")
+        {
+            if(collision.gameObject.GetComponent<EnemyMovement>().getVulnerable() == false)
+            {
+                health -= collision.gameObject.GetComponent<EnemyDamage>().damage;
+            }
+            Destroy(collision.gameObject);
+        }
+        if (collision.tag == "Bullet")
         {
             health -= collision.gameObject.GetComponent<EnemyDamage>().damage;
             Destroy(collision.gameObject);
