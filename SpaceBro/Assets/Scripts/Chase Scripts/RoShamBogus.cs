b70c686f9eaef4975eaf9d99a3a278f5cc5696f6
Unknown <chaseoqueso@gmail.com>
2019-05-14 16:43:51 -0700

    Player stuff and also Roshambogus stuff

---
+++ SpaceBro/Assets/Scripts/Chase Scripts/RoShamBogus.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoShamBogus : MonoBehaviour
+{
+    
+    public string state = "idle";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetLocation()
+    {
+
+    }
+
+    void SetSpeed()
+    {
+
+    }
+
+    void SetRock()
+    {
+        state = "rock";
+    }
+
+    void SetPaper()
+    {
+        state = "paper";
+    }
+
+    void SetScissors()
+    {
+        state = "scissors";
+    }
+}
---
+++ SpaceBro/Assets/Scripts/Chase Scripts/ScreenWrapVert.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenWrapVert : MonoBehaviour
+{
+    private float screenHeight;
+    private GameObject[] ghosts = new GameObject[2];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenHeight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0)).y - Camera.main.ScreenToWorldPoint(Vector3.zero).y;
+        CreateGhostShips();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Camera.main.WorldToViewportPoint(transform.position).y <= .25)
+        {
+            transform.Translate(new Vector3(0, screenHeight, 0));
+            ghosts[0].transform.Translate(new Vector3(0, screenHeight, 0));
+            ghosts[1].transform.Translate(new Vector3(0, screenHeight, 0));
+        }
+        if (Camera.main.WorldToViewportPoint(transform.position).y > .75)
+        {
+            transform.Translate(new Vector3(0, -screenHeight, 0));
+            ghosts[0].transform.Translate(new Vector3(0, -screenHeight, 0));
+            ghosts[1].transform.Translate(new Vector3(0, -screenHeight, 0));
+        }
+    }
+
+    void CreateGhostShips()
+    {
+        ghosts[0] = Instantiate(gameObject, transform.position - new Vector3(0, screenHeight, 0), Quaternion.identity) as GameObject;
+        ghosts[1] = Instantiate(gameObject, transform.position + new Vector3(0, screenHeight, 0), Quaternion.identity) as GameObject;
+        ghosts[0].transform.parent = transform.parent;
+        ghosts[1].transform.parent = transform.parent;
+        Destroy(ghosts[0].GetComponent<ScreenWrap>());
+        Destroy(ghosts[1].GetComponent<ScreenWrap>());
+        Destroy(ghosts[0].GetComponent<ScreenWrapVert>());
+        Destroy(ghosts[1].GetComponent<ScreenWrapVert>());
+    }
+}
